f07277b50faed5135d7fd1c183e0f6d824442942
RinaBoni <brisovae@mail.ru>
2023-05-09 14:32:41 +0900

    .

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,27 +1,32 @@
-import random as rand
+import random as random_number
+import matplotlib.pyplot as plt
+from numpy import array
 import numpy as np
 
-def MCint (f, a, b, n):
-    s = 0
-    for i in range (n):
-        x = rand.uniform(a,b)
-        s += f(x)
-    I = (float(b-a)/n)*s
-    return I
 
-def MCint_vec (f, a, b, n):
-    x = np.uniform(a, b, n)
-    s = sum(f(x))
-    I = (float(b-a)/n)*s
-    return I
-
-def MCint2 (f, a, b, n):
+def MCint3(f,a, b, n, N=100):
     s = 0
-    I = zeros(n)
+    I_values = []
+    k_values = []
+
     for k in range(1, n+1):
-        x = np.uniform(a, b)
+        x = random_number.uniform(a, b)
         s += f(x)
-        I[k-1] = (float(b-a)/k)*s
-    return I
+        if k % N == 0:
+            I = (float(b-a)/k)*s
+            I_values.append(I)
+            k_values.append(k)
+    return k_values, I_values
 
 
+def f1 (x):
+    return np.sin(x)/(1+np.sin(x))
+
+k, I = MCint3(f1, 0.8, 1.6, 1000000, N=10000)
+error = 6.5 - array(I)
+
+plt.title('Monte Carlo integration')
+plt.xlabel('n')
+plt.ylabel('error')
+plt.plot(k, error)
+plt.show()
